cee9534bc3ba78e627b83c5f867035c204813de1
Eduardo Cavazin <eduardotorcav@gmail.com>
2025-04-10 15:46:51 -0300

    Criação de Páginas

--- paginasweb/views.py
+++ paginasweb/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class IndexView(TemplateView):
+    template_name = "paginasweb/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Adicione dados ao contexto, se necessário
+        return context
