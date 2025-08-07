149227b9616236446c9eefa4517453591fa509c1
Eduardo Cavazin <eduardotorcav@gmail.com>
2025-08-07 20:00:00 -0300

    Usar SQLite novamente

--- financialAssistant/settings.py
+++ financialAssistant/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'financialAssistant.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres.ojvelwyztnshpgmhblcz",
-        "PASSWORD": "Duba1234!",
-        "HOST": "aws-0-sa-east-1.pooler.supabase.com",
-        "PORT": "6543",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "postgres",
+#         "USER": "postgres.ojvelwyztnshpgmhblcz",
+#         "PASSWORD": "Duba1234!",
+#         "HOST": "aws-0-sa-east-1.pooler.supabase.com",
+#         "PORT": "6543",
+#     }
+# }
 
 
 
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [ BASE_DIR / "static" ]
-STATIC_ROOT = "static_gcloud/"
+STATIC_ROOT = BASE_DIR / "static_gcloud/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
