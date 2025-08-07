8fcfcbca6211c146b3dc4659c4247d1e314791ad
Eduardo Cavazin <eduardotorcav@gmail.com>
2025-08-07 20:39:45 -0300

    Alteração de configuração para host confiável

--- financialAssistant/settings.py
+++ financialAssistant/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#3p=(_+@+ghilekvdo)!*c45ij4)s4gjct#jknd!u$azcmek0e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
